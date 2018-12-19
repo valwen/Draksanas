921efd1a2f862a62eeea0ce7dc31bf73cd39a7d8
DESKTOP-NTCHLRS\marie <marie.boland@hotmail.com>
2018-12-19 21:47:50 +0100

    Version 2

---
+++ Library/Collab/Base/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Choper la camera, multiplier par la normale du forward de la caméra (une unité dans la direction vecteur pos  - destination)
+    void Update()
+
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
---
+++ Library/Collab/Original/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour
+{
+    new public Transform camera;
+
+    //Choper la camera, multiplier par la normale du forward de la caméra
+    void Update()
+
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        Vector3 cameraF =camera.forward.normalized;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
